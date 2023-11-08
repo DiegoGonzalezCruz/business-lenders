8eecb5bb7eb3824539adabbf92f0e95086c56775
DiegoGonzalezCruz <diego@thinkey.cl>
2023-11-08 17:41:29 -0300

    fina build

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  // Optional: Change the output directory `out` -> `dist`
-  distDir: "dist",
-  images: {
-    unoptimized: true,
-  },
+  // output: "export",
+  // // Optional: Change the output directory `out` -> `dist`
+  // distDir: "dist",
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 module.exports = nextConfig;
